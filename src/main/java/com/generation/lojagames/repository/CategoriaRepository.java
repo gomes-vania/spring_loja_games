e0aebdce2fe99a601330b52195515f46dcaa9dc4
Vania Gomes <vania.vgomes@mercadolivre.com>
2022-12-13 19:04:14 -0300

    Update CategoriaRepository.java

--- src/main/java/com/generation/lojagames/repository/CategoriaRepository.java
+++ src/main/java/com/generation/lojagames/repository/CategoriaRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import com.generation.lojagames.model.Categoria;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
 	public List <Categoria> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
